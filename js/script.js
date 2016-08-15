92ba46792695ec20df282ca15032c407a3edbf36
Tyler Krupicka <tylerkrupicka@gmail.com>
2016-08-14 23:11:59 -0400

    added expand animation, footer, and more content

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
     $('.card-list-element').on('click', function() {
-        $(this).find('.card-list-text').fadeIn(300);
+        var hidden = $(this).find('.card-hidden');
+        var text = $(this).find('.card-list-text');
+        if ($(hidden).is(":visible")) {
+            $(text).animate({opacity: "0"},100,function(){
+                    $(hidden).slideUp(200);
+                }
+            );
+        } else {
+            $(hidden).slideDown(200, function(){
+                $(text).animate({opacity: "1"},100);
+            });
+        }
     });
 });
